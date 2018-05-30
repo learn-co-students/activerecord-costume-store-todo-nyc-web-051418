6184c5623b2cd26b7a4c6a5df96b142805b46694
github username <github email address>
2018-05-30 13:13:06 -0400

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,25 @@
-# Create your haunted_houses migration here
+# The haunted_houses table will have eight columns:
+#
+# name
+# location
+# theme
+# price
+# whether they're family friendly or not
+# opening date
+# closing date
+# long description
+
+class CreateHauntedHouses < ActiveRecord::Migration[4.2]
+
+  create_table :haunted_houses do |t|
+      t.string :name
+      t.integer :location
+      t.string :theme
+      t.float :price
+      t.boolean :family_friendly
+      t.datetime :opening_date
+      t.datetime :closing_date
+      t.string :description
+    end
+
+end
